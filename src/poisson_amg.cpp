2358ea4efc3df6d75700344c7bc2cf25f3bab5e3
Trygve Bærland <trygve.baerland@gmail.com>
2017-04-06 16:23:52 +0200

    Started on Poisson parallel solver.

---
+++ src/poisson_amg.cpp
@@ -0,0 +1,13 @@
+// Program for solving Poisson's equation with HYPRE.
+
+#include "mfem.hpp"
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+using namespace mfem;
+
+int main(int argc, char *argv[]){
+
+
+}
